25d2a6835fafddb385459944cb2aeae71f3471f1
jihopark <lastspurt17@gmail.com>
2015-10-26 15:23:28 +0800

    Copied same code to index.android.js and completed build

--- index.android.js
+++ index.android.js
@@ -7,46 +7,109 @@
 var React = require('react-native');
 var {
   AppRegistry,
+  ListView,
   StyleSheet,
   Text,
   View,
 } = React;
 
+var API_DOMAIN = 'https://plainexchange.herokuapp.com/api/v1/';
+
 var plainXiOS = React.createClass({
+  APIRoute: "offer",
+  getInitialState: function() {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    fetch(API_DOMAIN + this.APIRoute)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData),
+          loaded: true,
+        });
+      })
+      .done();
+  },
+
   render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderOffer}
+        style={styles.listView}
+      />
+    );
+  },
+
+  renderLoadingView: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
+        <Text>
+          Loading offers...
         </Text>
       </View>
     );
-  }
+  },
+  renderOffer: function(offer) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.seller}>Seller {offer["Seller"]}</Text>
+        <View style={styles.rightContainer}>
+          <Text style={styles.number}>Amount Have: {offer["AmountHave"]}</Text>
+          <Text style={styles.currency}>{offer["Have"]}</Text>
+          <Text style={styles.number}>Amount Need: {offer["AmountNeed"]}</Text>
+          <Text style={styles.currency}>{offer["Need"]}</Text>
+        </View>
+      </View>
+    );
+  },
 });
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  seller: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
   },
-  instructions: {
+  rightContainer: {
+    flex: 1,
+  },
+  currency: {
+    fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  },
+  number: {
+    fontSize: 25,
+  },
+  year: {
+    textAlign: 'center',
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
   },
 });
+
 
 AppRegistry.registerComponent('plainXiOS', () => plainXiOS);
