3c95ce544cbed8784eda3b9eb8bcc1417f19f2eb
jihopark <lastspurt17@gmail.com>
2015-11-03 13:57:50 +0800

    Configured Parse to iOS

--- index.ios.js
+++ index.ios.js
@@ -3,12 +3,41 @@
 var React = require('react-native');
 var {
   AppRegistry,
+  PushNotificationIOS,
+  AlertIOS,
 } = React;
 
 var API_DOMAIN = 'https://plainexchange.herokuapp.com/api/v1/';
 
 var OffersList = require('./components/OffersList.js')
 var body = React.createClass({
+  _onCheckPermission: function(permission) {
+    console.log("Permission alert:" + permission.alert + " badge:" + permission.badge + " sound:" + permission.sound)
+    if (!permission.alert) {
+      console.log("Request Permission");
+      PushNotificationIOS.requestPermissions();
+    }
+    else {
+      console.log("PushNotification add listener");
+      PushNotificationIOS.addEventListener("notification", this._onNotification);
+    }
+  },
+  _onNotification: function(notification) {
+    AlertIOS.alert(
+     'Notification Received',
+     'Alert message: ' + notification.getMessage(),
+     [{
+       text: 'Dismiss',
+       onPress: null,
+     }]
+   );
+  },
+  componentDidMount: function() {
+    PushNotificationIOS.checkPermissions(this._onCheckPermission);
+  },
+  componentWillUnmount: function() {
+    PushNotificationIOS.removeEventListener('notification', this._onNotification);
+  },
   render: function() {
     return (
       <OffersList API_DOMAIN={API_DOMAIN}/>
--- ios/plainX/AppDelegate.m
+++ ios/plainX/AppDelegate.m
@@ -11,11 +11,49 @@
 
 #import "RCTRootView.h"
 
+#import "RCTPushNotificationManager.h"
+
+#import <Parse/Parse.h>
+
+
 @implementation AppDelegate
+
+// Required for the register event.
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+[RCTPushNotificationManager application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+// Store the deviceToken in the current installation and save it to Parse.
+PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+[currentInstallation setDeviceTokenFromData:deviceToken];
+currentInstallation.channels = @[ @"global" ];
+[currentInstallation saveInBackground];
+
+}
+// Required for the notification event.
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)notification
+{
+[RCTPushNotificationManager application:application didReceiveRemoteNotification:notification];
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
+
+  //For Parse
+  // Initialize Parse.
+  [Parse setApplicationId:@"AbGNATEXLxsdDAPalV5Qlh8uncqdzo44Gm8som0S"
+                clientKey:@"iKyk3OAQZuuAIN5n8Bj0h8UT4e43vxztR516tFpN"];
+
+  // [Optional] Track statistics around application opens.
+  [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+
+  UIUserNotificationType userNotificationTypes = (UIUserNotificationTypeAlert |
+                                                  UIUserNotificationTypeBadge |
+                                                  UIUserNotificationTypeSound);
+  UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:userNotificationTypes
+                                                                           categories:nil];
+  [application registerUserNotificationSettings:settings];
+  [application registerForRemoteNotifications];
 
   /**
    * Loading JavaScript code - uncomment the one you want.
@@ -31,7 +69,7 @@
    * on the same Wi-Fi network.
    */
 
-  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+  jsCodeLocation = [NSURL URLWithString:@"http://10.89.137.229:8081/index.ios.bundle?platform=ios&dev=true"];
 
   /**
    * OPTION 2
