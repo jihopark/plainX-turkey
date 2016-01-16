35a71779c5578dcb9975a6ae8d59d5d526af7483
Long Hoang <long@mindworker.de>
2016-01-16 19:57:54 +0800

    Vertical center align button text in CardButton

--- components/CardButton.js
+++ components/CardButton.js
@@ -9,27 +9,31 @@ var {
 
 var CardButton = React.createClass({
   render: function() {
-    return (
-      <Text style={styles.cardButton}>
-        {this.props.text}
-      </Text>
-    );
+      return (
+	  <React.View style={styles.parent}>
+            <Text style={styles.cardButton}>
+              {this.props.text}
+	    </Text>
+	  </React.View>
+      );
   }
 });
 
 var styles = StyleSheet.create({
-  cardButton: {
-    flex: 1,
-    fontSize: 60/3,
-    textAlign: 'center',
-    color: 'white',
-    backgroundColor: '#33cc66',
-    borderRadius: 2,
-    overflow:'hidden',
-    paddingTop:20,
-    paddingBottom:20,
-    marginTop: 10,
-  },
+    parent: {
+	backgroundColor: '#33cc66',
+	borderRadius: 2,
+	overflow:'hidden',
+	marginTop: 10,
+	paddingTop:20,
+	paddingBottom:20,
+    },
+    cardButton: {
+	flex: 1,
+	textAlign: 'center',
+	color: 'white',
+	fontSize: 60/3,
+    },
 });
 
 module.exports = CardButton;
