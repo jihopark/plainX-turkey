a0b4bfd1b5e87b871e90d3136835d082c45f3e70
jihopark <lastspurt17@gmail.com>
2016-01-20 11:31:47 +0800

    Change IP adress to run on device

--- ios/plainX/AppDelegate.m
+++ ios/plainX/AppDelegate.m
@@ -71,7 +71,7 @@ UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTy
    * on the same Wi-Fi network.
    */
 
-  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+  jsCodeLocation = [NSURL URLWithString:@"http://10.89.160.133:8081/index.ios.bundle?platform=ios&dev=true"];
 
   /**
    * OPTION 2
