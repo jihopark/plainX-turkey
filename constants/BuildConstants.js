237b5233cce722a9d7ada41305270d50a424d8bc
jihopark <lastspurt17@gmail.com>
2016-01-26 18:46:40 +0800

    put temporary domain for android

--- constants/BuildConstants.js
+++ constants/BuildConstants.js
@@ -7,7 +7,7 @@ const BUILD = PRODUCTION;
 
 var BuildConstants = {
   API_DOMAIN: BUILD == DEVELOPMENT ?
-    'https://plaindev.herokuapp.com/api/v1/' : 'https://plainexchange.herokuapp.com/api/v1/',
+    'https://plaindev.herokuapp.com/api/v1/' : 'https://plainx-android.herokuapp.com/api/v1/',
   MIXPANEL_TOKEN: BUILD == DEVELOPMENT ?
     "b96294c2f2d4ba22307fc1396251b62a" : "04885f324faf84460d4b8418cd52eac6",
 };
