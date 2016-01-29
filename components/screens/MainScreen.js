2d81786102a1aa3c9e46a4db08afe69c367f112a
jihopark <lastspurt17@gmail.com>
2016-01-29 19:40:13 +0800

    Load MainScreen when back from inactive

--- components/screens/MainScreen.js
+++ components/screens/MainScreen.js
@@ -8,6 +8,7 @@ var {
   TouchableOpacity,
   Platform,
   AsyncStorage,
+  AppStateIOS,
 } = React;
 
 var Routes = require('../screens/Routes.js');
@@ -19,6 +20,8 @@ var PlainListView = require('../PlainListView.js');
 var PlainLog = require('../../PlainLog.js');
 var P = new PlainLog("MainScreen");
 
+var ActivityAndroid = require('react-native-activity-android');
+
 
 class MainScreen extends BaseScreen{
   constructor(props) {
@@ -26,7 +29,51 @@ class MainScreen extends BaseScreen{
     this.endPoint = "main";
     this.pushConversationsScreen = this.pushConversationsScreen.bind(this);
     this.renderScreen = this.renderScreen.bind(this);
+    this.handleActivityPause = this.handleActivityPause.bind(this);
+    this.handleActivityResume = this.handleActivityResume.bind(this);
+    this.handleAppStateChange = this.handleAppStateChange.bind(this);
     this.trackName = "Main";
+    this.appState = 'active';
+  }
+
+  componentDidMount() {
+    super.componentDidMount();
+    if (Platform.OS == 'ios')
+      AppStateIOS.addEventListener('change', this.handleAppStateChange);
+    else{
+      ActivityAndroid.addEventListener('activityPause', this.handleActivityPause);
+      ActivityAndroid.addEventListener('activityResume', this.handleActivityResume);
+    }
+  }
+
+  componentWillUnmount() {
+    super.componentWillUnmount();
+    if (Platform.OS == 'ios')
+      AppStateIOS.removeEventListener('change', this.handleAppStateChange);
+    else{
+      ActivityAndroid.removeEventListener('activityPause', this.handleActivityPause);
+      ActivityAndroid.removeEventListener('activityResume', this.handleActivityResume);
+    }
+  }
+
+  handleActivityPause(){
+    P.log("handleActivityPause", "inactive");
+    this.handleAppStateChange('inactive');
+  }
+
+  handleActivityResume(){
+    P.log("handleActivityResume", "active");
+    this.handleAppStateChange('active');
+  }
+
+  handleAppStateChange(appState) {
+    if (this.appState != appState) {
+      this.appState = appState;
+      if (appState == 'active') {
+        this.setState({data:null});
+        this.loadScreen();
+      }
+    }
   }
 
   pushConversationsScreen(event) {
